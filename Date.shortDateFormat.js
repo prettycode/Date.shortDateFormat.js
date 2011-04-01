d53c5ced53123ec7f3265d8adab942330d1c3338
Chris O <chris@prettycode.org>
2011-03-31 20:19:29 -0700

    Removing unnecessary formatting.

--- Date.shortDateFormat.js
+++ Date.shortDateFormat.js
@@ -11,108 +11,104 @@
     are minified.
 */
 
-(function () {
+(function() {
 
-    (function() {
+    function ToShortDateFormatError(msg) {
+        return "Argument `format` is malformed: " + msg;
+    }
 
-        function ToShortDateFormatError(msg) {
-            return "Argument `format` is malformed: " + msg;
-        }
-
-        function contains(s, search) {
-            return s.indexOf(search) !== -1;
-        };
-        
-        // Convert Date object to formatted short date string
-
-        Date.prototype.toShortDateFormat = function(format) {
-        
-            format = format || Date.shortDateFormat;
-
-            var month = this.getMonth() + 1; // Months (only) start at 0
-
-            if (contains(format, "mm")) {
-                format = format.replace("mm", (month < 10 ? "0" : "") + month);
-            }
-            else if (contains(format, "m")) {
-                format = format.replace("m", month);
-            }
-            else {
-                throw ToShortDateFormatError("missing `m` or `mm` for month");
-            }
-
-            var day = this.getDate();
-
-            if (contains(format, "dd")) {
-                format = format.replace("dd", (day < 10 ? "0" : "") + day);
-            }
-            else if (contains(format, "d")) {
-                format = format.replace("d", day);
-            }
-            else {
-                throw ToShortDateFormatError("missing `d` or `dd` for day");
-            }
-
-            if (contains(format, "yyyy")) {
-                format = format.replace("yyyy", this.getFullYear());
-            }
-            else if (contains(format, "yy")) {
-                format = format.replace("yy", (this.getFullYear() + "").substring(2));
-            }
-            else {
-                throw ToShortDateFormatError("missing `yyyy` or `yy` for year");
-            }
-
-            return format;
-        };
+    function contains(s, search) {
+        return s.indexOf(search) !== -1;
+    };
     
-    })();
+    // Convert Date object to formatted short date string
+
+    Date.prototype.toShortDateFormat = function(format) {
     
-     // Convert localized short date string to Date object
+        format = format || Date.shortDateFormat;
 
-    Date.parseShortDateFormat = function (format, date) {
+        var month = this.getMonth() + 1; // Months (only) start at 0
 
-        var separator = date.replace(/\d/g, "").charAt(0),
-            formatSplit = format.split(separator),
-            dateSplit = date.split(separator);
-
-        var day, month, year;
-        
-        for (var i = 0, len = formatSplit.length; i < len; i++) {
-        
-            var datePart = formatSplit[i].charAt(0);
-
-            if (datePart === "d") {
-                day = dateSplit[i];
-            }
-            else if (datePart === "m") {
-                month = dateSplit[i];
-            }
-            else if (datePart === "y") {
-                year = dateSplit[i];
-            }
+        if (contains(format, "mm")) {
+            format = format.replace("mm", (month < 10 ? "0" : "") + month);
         }
-        
-        month = parseInt(month, 10);
-
-        if (isNaN(month) || month > 12 || month < 1) {
-            return null;
+        else if (contains(format, "m")) {
+            format = format.replace("m", month);
+        }
+        else {
+            throw ToShortDateFormatError("missing `m` or `mm` for month");
         }
 
-        day = parseInt(day, 10);
+        var day = this.getDate();
 
-        if (isNaN(day) || day > 31 || day < 1) {
-            return null;
+        if (contains(format, "dd")) {
+            format = format.replace("dd", (day < 10 ? "0" : "") + day);
+        }
+        else if (contains(format, "d")) {
+            format = format.replace("d", day);
+        }
+        else {
+            throw ToShortDateFormatError("missing `d` or `dd` for day");
         }
 
-        year = parseInt(year, 10);
-
-        if (isNaN(year)) {
-            return null;
+        if (contains(format, "yyyy")) {
+            format = format.replace("yyyy", this.getFullYear());
+        }
+        else if (contains(format, "yy")) {
+            format = format.replace("yy", (this.getFullYear() + "").substring(2));
+        }
+        else {
+            throw ToShortDateFormatError("missing `yyyy` or `yy` for year");
         }
 
-        return new Date(month + "/" + day + "/" + year);
-        
+        return format;
     };
 
 })();
+    
+// Convert localized short date string to Date object
+
+Date.parseShortDateFormat = function (format, date) {
+
+    var separator = date.replace(/\d/g, "").charAt(0),
+        formatSplit = format.split(separator),
+        dateSplit = date.split(separator);
+
+    var day, month, year;
+    
+    for (var i = 0, len = formatSplit.length; i < len; i++) {
+    
+        var datePart = formatSplit[i].charAt(0);
+
+        if (datePart === "d") {
+            day = dateSplit[i];
+        }
+        else if (datePart === "m") {
+            month = dateSplit[i];
+        }
+        else if (datePart === "y") {
+            year = dateSplit[i];
+        }
+    }
+    
+    month = parseInt(month, 10);
+
+    if (isNaN(month) || month > 12 || month < 1) {
+        return null;
+    }
+
+    day = parseInt(day, 10);
+
+    if (isNaN(day) || day > 31 || day < 1) {
+        return null;
+    }
+
+    year = parseInt(year, 10);
+
+    if (isNaN(year)) {
+        return null;
+    }
+
+    return new Date(month + "/" + day + "/" + year);
+    
+};
